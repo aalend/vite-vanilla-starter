c06274a0000388e7d828a717b96c4c21cd48f735
aalend <aalendemirov@gmail.com>
2022-08-02 12:53:27 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
+
+export default defineConfig({
+  plugins: [
+    // https://github.com/feat-agency/vite-plugin-webfont-dl
+    ViteWebfontDownload([
+      'https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700;800;900&display=swap',
+    ]),
+  ],
+});
